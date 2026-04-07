0e306bd737a63a85bb2d1e1648731fea108e2804
lasya-railtelit <maddipatisrilasya@gmail.com>
2026-04-07 17:38:07 +0530

    mobile view changes

--- src/pages/InfoPage/InfoPage.js
+++ src/pages/InfoPage/InfoPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import './InfoPage.css';
 
 const PAGE_CONTENT = {
